1778ccab8a41c6f61587edf25a1928c3367a4b1a
tobiasruck <ruck.tobias@gmail.com>
2020-08-03 18:29:03 +0200

    Add time measurement for tx building

--- iguana-ws/src/lib.rs
+++ iguana-ws/src/lib.rs
@@ -4,6 +4,7 @@ use tokio::runtime::Runtime;
 use futures::{StreamExt, SinkExt};
 use warp::{Filter, filters::ws::Message};
 use std::sync::RwLock;
+use std::time::Instant;
 use lazy_static::lazy_static;
 
 lazy_static! {
@@ -13,8 +14,15 @@ lazy_static! {
 pub fn run_iguana_ws(make_tx: impl Fn() -> Result<UnhashedTx, Error>) {
     pretty_env_logger::init();
 
+    let t0 = Instant::now();
     let tx = make_tx().expect("Failed building transaction:");
+    let dt_build_tx = t0.elapsed().as_micros();
+    let t1 = Instant::now();
     *JSON.write().unwrap() = tx_to_json(&tx).unwrap();
+    let dt_json_tx = t1.elapsed().as_micros();
+
+    println!("dt build: {}ms", dt_build_tx as f64 / 1000.0);
+    println!("dt json: {}ms", dt_json_tx as f64 / 1000.0);
 
     let mut rt = Runtime::new().expect("Failed setting up runtime");
 
@@ -24,7 +32,7 @@ pub fn run_iguana_ws(make_tx: impl Fn() -> Result<UnhashedTx, Error>) {
             .map(|ws: warp::ws::Ws| {
                 ws.on_upgrade(|websocket| {
                     async {
-                        println!("Sent tx.");
+                        println!("Sending tx.");
                         let (mut outgoing, mut incoming) = websocket.split();
                         let json_text = JSON.read().unwrap().clone();
                         if let Err(err) = outgoing.send(Message::text(json_text)).await {
